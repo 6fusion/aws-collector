d1e82403cb55f89abeaf4666035ee37a3d7ca0ac
Kirill Turutin <turutink@yandex.ru>
2016-11-15 12:04:37 +0300

    Modules structure fix in aws_price_list

--- lib/billing/aws_price_list.rb
+++ lib/billing/aws_price_list.rb
@@ -1,17 +1,17 @@
 module AWS
   module PriceList
     REGIONS = {
-      "us-east-1" => "US East (N. Virginia)",
-      "us-west-2" => "US West (Oregon)",
-      "us-west-1" => "US West (N. California)",
-      "eu-west-1" => "EU (Ireland)",
-      "eu-central-1" => "EU (Frankfurt)",
-      "ap-southeast-1" => "Asia Pacific (Singapore)",
-      "ap-northeast-1" => "Asia Pacific (Tokyo)",
-      "ap-southeast-2" => "Asia Pacific (Sydney)",
-      "ap-northeast-2" => "Asia Pacific (Seoul)",
-      "ap-south-1" => "Asia Pacific (Mumbai)",
-      "sa-east-1" => "South America (São Paulo)"
+        "us-east-1" => "US East (N. Virginia)",
+        "us-west-2" => "US West (Oregon)",
+        "us-west-1" => "US West (N. California)",
+        "eu-west-1" => "EU (Ireland)",
+        "eu-central-1" => "EU (Frankfurt)",
+        "ap-southeast-1" => "Asia Pacific (Singapore)",
+        "ap-northeast-1" => "Asia Pacific (Tokyo)",
+        "ap-southeast-2" => "Asia Pacific (Sydney)",
+        "ap-northeast-2" => "Asia Pacific (Seoul)",
+        "ap-south-1" => "Asia Pacific (Mumbai)",
+        "sa-east-1" => "South America (São Paulo)"
     }
 
     def self.find(criteria)
@@ -20,43 +20,43 @@ module AWS
 
       term = Term.find_by(id: "AmazonEC2:OnDemand:#{product.sku}").data
       cost_per_hour = decode(term).
-                      first.last["priceDimensions"].
-                      first.last["pricePerUnit"].
-                      first.last
+          first.last["priceDimensions"].
+          first.last["pricePerUnit"].
+          first.last
 
       return unless cost_per_hour
 
-      { cost_per_hour: cost_per_hour,
-        billing_resource: "AWS Price List API" }.to_dot
+      {cost_per_hour: cost_per_hour,
+       billing_resource: "AWS Price List API"}.to_dot
     end
-  end
 
-  def self.decode(json)
-    JSON.parse(json.to_json.gsub(/%2E/, "."))
-  end
+    def self.decode(json)
+      JSON.parse(json.to_json.gsub(/%2E/, "."))
+    end
 
-  module EC2
-    def self.price_details(options)
-      region = options[:region]
-      instance_type = options[:instance_type]
-      operating_system = options[:operating_system]
-      ebs_optimized = options[:ebs_optimized]
-      tenancy = options[:tenancy]
+    module EC2
+      def self.price_details(options)
+        region = options[:region]
+        instance_type = options[:instance_type]
+        operating_system = options[:operating_system]
+        ebs_optimized = options[:ebs_optimized]
+        tenancy = options[:tenancy]
 
-      criteria = {
-        "offer_code" => "AmazonEC2",
-        "product_family" => "Compute Instance",
-        "attr.preInstalledSw" => "NA",
-        "attr.location" => REGIONS[region],
-        "attr.instanceType" => instance_type,
-        "attr.operatingSystem" => operating_system,
-        "attr.tenancy" => tenancy == "default" ? "Shared" : tenancy
-      }
+        criteria = {
+            "offer_code" => "AmazonEC2",
+            "product_family" => "Compute Instance",
+            "attr.preInstalledSw" => "NA",
+            "attr.location" => REGIONS[region],
+            "attr.instanceType" => instance_type,
+            "attr.operatingSystem" => operating_system,
+            "attr.tenancy" => tenancy == "default" ? "Shared" : tenancy
+        }
 
-      criteria["attr.ebsOptimized"] = "Yes" if ebs_optimized
-      criteria["attr.licenseModel"] = "License Included" if operating_system == "Windows"
+        criteria["attr.ebsOptimized"] = "Yes" if ebs_optimized
+        criteria["attr.licenseModel"] = "License Included" if operating_system == "Windows"
 
-      AWS::PriceList.find(criteria)
+        AWS::PriceList.find(criteria)
+      end
     end
 
     module EBS
@@ -74,10 +74,10 @@ module AWS
         end
 
         criteria = {
-          "offer_code" => "AmazonEC2",
-          "product_family" => "Storage",
-          "attr.location" => REGIONS[region],
-          "attr.usagetype" => /EBS:VolumeUsage#{ebs_type}/i
+            "offer_code" => "AmazonEC2",
+            "product_family" => "Storage",
+            "attr.location" => REGIONS[region],
+            "attr.usagetype" => /EBS:VolumeUsage#{ebs_type}/i
         }
 
         AWS::PriceList.find(criteria)
