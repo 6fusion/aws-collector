ca0c245aa7383e5d0a6c4f3f6e9f212964b48c2d
Peyton Vaughn <pvaughn@6fusion.com>
2017-05-11 12:35:47 -0400

    wip

--- lib/models/inventory/inventory.rb
+++ lib/models/inventory/inventory.rb
@@ -48,8 +48,8 @@ class Inventory
     stats = Hash.new{|h,k| h[k]=0 }
     stats[:name] = "aggregated instance volume"
     volumes.each{|volume|
-      stats[:storage_bytes] += volume.bytes,
-      stats[:speed_bits_per_second] += PropertyHelper.default_disk_io.to_i,
+      stats[:storage_bytes] += volume.bytes
+      stats[:speed_bits_per_second] += PropertyHelper.default_disk_io.to_i
       stats[:cost_per_hour] += volume.cost
     }
     stats
